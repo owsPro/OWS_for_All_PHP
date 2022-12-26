59bdd8b11eb28626077a1586bcf0a5171862e22a
owsPro <elm@eadress.de>
2022-12-26 21:00:45 +0100

    Fix: Cross-site Scripting (XSS)

--- js/wsbase.js
+++ js/wsbase.js
@@ -2,28 +2,28 @@
 
   This file is part of OpenWebSoccer-Sim.
 
-  OpenWebSoccer-Sim is free software: you can redistribute it 
-  and/or modify it under the terms of the 
-  GNU Lesser General Public License 
+  OpenWebSoccer-Sim is free software: you can redistribute it
+  and/or modify it under the terms of the
+  GNU Lesser General Public License
   as published by the Free Software Foundation, either version 3 of
   the License, or any later version.
 
   OpenWebSoccer-Sim is distributed in the hope that it will be
   useful, but WITHOUT ANY WARRANTY; without even the implied
-  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public 
-  License along with OpenWebSoccer-Sim.  
+  You should have received a copy of the GNU Lesser General Public
+  License along with OpenWebSoccer-Sim.
   If not, see <http://www.gnu.org/licenses/>.
 
 ******************************************************/
 $(function() {
-	
+
 	var WSCONFIG = {
 			AJAX_URL: "ajax.php"
 	};
-	
+
 	/**
 	 * percent value setting sliders
 	 */
@@ -32,7 +32,7 @@ $(function() {
 			$("input.slider").attr('value', ev.value);
 		});
 	});
-	
+
 	/**
 	 * Initializations of general components.
 	 */
@@ -41,14 +41,14 @@ $(function() {
 		$(".wspopover").popover();
 		initAutoComplete();
 	};
-	
+
 	/**
 	 * autocomplete AJAX support
 	 */
 	var initAutoComplete = function() {
 		$(".autocomplete").typeahead({
 			minLength: 2,
-			
+
 			source: function(query, process) {
 		        return $.ajax({
 		            url: WSCONFIG.AJAX_URL,
@@ -62,40 +62,40 @@ $(function() {
 		    }
 		});
 	};
-	
+
 	/**
 	 * AJAXified forms
 	 */
 	$(document).on("click", ".ajaxSubmit", function(e) {
 		e.preventDefault();
-		
+
 		var form = $(this).closest("form");
 		form.submit(function() {
 			  return false;
 		});
-		
+
 		ajaxHandler(form.serialize(), $(this).data("ajaxtarget"), $(this).data("ajaxblock"), $(this).data("messagetarget"), form, $(this).data("ignoreemptymessages"));
 	});
-	
+
 	/**
 	 * AJAXified links
 	 */
 	$(document).on("click", ".ajaxLink", function(e) {
 		e.preventDefault();
-		
+
 		var targetId = $(this).data("ajaxtarget");
 		if (!$(this).data("ajaxloaded") || $(this).data("ajaxdisabledcache")) {
-			ajaxHandler($(this).data("ajaxquerystr"), targetId, $(this).data("ajaxblock"), $(this).data("messagetarget"), 
+			ajaxHandler($(this).data("ajaxquerystr"), targetId, $(this).data("ajaxblock"), $(this).data("messagetarget"),
 					$("#" + targetId).closest("div"), $(this).data("ignoreemptymessages"));
-			
+
 			// cache only if area is not updated by any other link
 			if ($("a[data-ajaxtarget='" + targetId + "']").length < 2) {
 				$(this).data("ajaxloaded", "1");
 			}
-			
+
 		}
 	});
-	
+
 	var ajaxHandler = function(queryString, targetId, blockId, messagesTargetId, blockedElement, ignoreemptymessages) {
 		if (!blockId) {
 			blockId = "";
@@ -111,20 +111,20 @@ $(function() {
 				}
 			})
 			.done(function(data) {
-				$("#" + targetId).html(data.content);
-				
+				$("#" + targetId).html(unescape(encodeURIComponent(data.content)));
+
 				if (!ignoreemptymessages || data.messages.trim().length) {
 					var msgTargetId = (messagesTargetId) ? messagesTargetId : "messages";
-					$("#" + msgTargetId).html(data.messages);
+					$("#" + msgTargetId).html(unescape(encodeURIComponent(data.messages)));
 				}
-				
+
 			})
-			.always(function() { 
+			.always(function() {
 				blockedElement.unblock();
 				ajaxLoader.hide();
 			});
 	};
-	
+
 	// enable browser history after AJAX link
 	$(document).ready(function() {
 		var hash = location.hash.replace('#', '');
@@ -132,18 +132,18 @@ $(function() {
 			$("a.ajaxLink[href$='#" + hash + "']").click();
 		}
 	});
-	
+
 	// enable browser history for tab panes
 	$('a[data-toggle="tab"]').on('shown', function (e) {
 		  window.location.hash = e.target.hash;
 	});
-	
+
 	// enable client side "active" marker of nav items
 	$(document).on('click', '.clientsideNavItem', function (e) {
 		$(this).parent().find("> .active").removeClass("active");
         $(this).addClass("active");
 	});
-	
+
 	/**
 	 * Init Countdown
 	 */
@@ -164,12 +164,12 @@ $(function() {
 			}
 		});
 	});
-	
+
 	/**
 	 * init notifications popup
 	 */
-	$("#notificationsLink").popover({ 
-	    html: true, 
+	$("#notificationsLink").popover({
+	    html: true,
 	    placement: "bottom",
 	    content: function() {
 	    	var contentHtml = $("#notificationspopupwrapper").html();
@@ -177,12 +177,12 @@ $(function() {
 	    	return contentHtml;
 	    }
 	});
-	
+
 	var triggerAjaxLinksOnLoad = function() {
 		$(".triggerClickOnLoad:not(.clicked)").trigger("click");
 		$(".triggerClickOnLoad").addClass("clicked");
 	};
-	
+
 	var triggerAjaxLoadOfBlocks = function() {
 		$(".ajaxLoadedBlock").each(function() {
 			var queryStr = $(this).data("ajaxquerystr");
@@ -192,35 +192,35 @@ $(function() {
 			var elementToBlock = $(this);
 			var ignoreEmptyMessages = $(this).data("ignoreemptymessages");
 			var refreshPeriod = $(this).data("refreshperiod");
-			
-			ajaxHandler(queryStr, elementId, blockId, messagesTarget, 
+
+			ajaxHandler(queryStr, elementId, blockId, messagesTarget,
 					elementToBlock, ignoreEmptyMessages);
-			
+
 			// refresh after X seconds
 			if (refreshPeriod) {
 				setInterval(function () {
-					ajaxHandler(queryStr, elementId, blockId, messagesTarget, 
+					ajaxHandler(queryStr, elementId, blockId, messagesTarget,
 							elementToBlock, ignoreEmptyMessages);
 			    }, refreshPeriod * 1000);
 			}
-			
+
 		});
-		
+
 	};
-	
+
 	// init components which can be re-rendered on AJX calls
 	$(document).ready(function() {
 		initComponents();
 		triggerAjaxLinksOnLoad();
 		triggerAjaxLoadOfBlocks();
-		
+
 	});
 	$(document).ajaxComplete(function() {
 		initComponents();
 		blockMatchRefreshButton();
 		triggerAjaxLinksOnLoad();
 	});
-	
+
 	/**
 	 * Block AJAX refresh button at match reports
 	 */
@@ -228,7 +228,7 @@ $(function() {
 	var blockMatchRefreshButton = function() {
 		$("#matchReportRefresh").each(function() {
 			var timeToBlock = $(this).data("blockseconds");
-			
+
 			if (!refreshCountdownStarted) {
 				$(this).each(function() {
 			        $(this).attr('disabled', 'disabled');
@@ -242,15 +242,15 @@ $(function() {
 			                countdownElement.text('');
 			                refreshCountdownStarted = false;
 			                clearInterval(interval);
-			                
+
 			                // automatic refresh
 			                $("#matchReportRefresh").trigger("click");
 			            }
 			        }, 1000);
 			    });
 			}
-			
+
 		});
 	};
-	
+
 });
