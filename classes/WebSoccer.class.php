32bc405229b6e7b59c6d813a0c8a1c18fcfb4590
owsPro <elm@eadress.de>
2022-12-28 04:52:13 +0100

    Security Fix & new Function
    - new function Browser Language

--- classes/WebSoccer.class.php
+++ classes/WebSoccer.class.php
@@ -239,6 +239,33 @@ class LoginCheck{
 		$hash=$this->crypt_private($password,$stored_hash);
 		if($hash[0]==='*')$hash=crypt($password,$stored_hash);
 		return$hash===$stored_hash;}}
+/**
+ * Browsersprache ermitteln
+ *
+ * $i18n = I18n::getInstance($website->getConfig('supported_languages'));
+ * $lang = BrowserLanguage ($i18n->getCurrentLanguage(), 'en', null, false);
+ */
+function BrowserLanguage($allowed_languages, $default_language, $lang_variable = null, $strict_mode = true) {
+	if ($lang_variable === null) $lang_variable = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+	if (empty($lang_variable)) return $default_language;
+	$accepted_languages = preg_split('/,\s*/', $lang_variable);
+	$current_lang = $default_language;
+	$current_q = 0;
+	foreach ($accepted_languages as $accepted_language) {
+		$res = preg_match ('/^([a-z]{1,8}(?:-[a-z]{1,8})*)(?:;\s*q=(0(?:\.[0-9]{1,3})?|1(?:\.0{1,3})?))?$/i', $accepted_language, $matches);
+		if (!$res) continue;
+		$lang_code = explode ('-', $matches[1]);
+		if (isset($matches[2]))     $lang_quality = (float)$matches[2];
+		else $lang_quality = 1.0;
+		while (count ($lang_code)) {
+			if (in_array (strtolower (join ('-', $lang_code)), $allowed_languages)) {
+				if ($lang_quality > $current_q) {
+					$current_lang = strtolower (join ('-', $lang_code));
+					$current_q = $lang_quality;
+					break;}}
+			if ($strict_mode) break;
+			array_pop ($lang_code);}}
+	return $current_lang;}
 //+ Mudule: actionlogs
 //+ Mudule: alltimetable
 //+ Mudule: cancellation
@@ -827,10 +854,7 @@ class DataUpdateSimulatorObserver {
 		if (!$isNationalTeam && $playerinfo['lending_matches'] > 0) $this->handleBorrowedPlayer($columns, $playerinfo);
 		$this->_db->queryUpdate($columns, $fromTable, $whereCondition, $parameters);}
 	function deductSalary(SimulationTeam $team, $salary) {
-		BankAccountDataService::debitAmount($this->_websoccer, $this->_db, $team->id,
-			$salary,
-			'match_salarypayment_subject',
-			'match_salarypayment_sender');}
+		BankAccountDataService::debitAmount($this->_websoccer, $this->_db, $team->id, $salary, 'match_salarypayment_subject', 'match_salarypayment_sender');}
 	function updateTeams(SimulationMatch $match) {
 		$fromTable = $this->_websoccer->getConfig('db_prefix') . '_verein';
 		$whereCondition = 'id = %d';
