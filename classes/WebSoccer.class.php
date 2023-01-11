7542273dcb563e45d247c35c8e4e14fa2a61c120
owsPro <elm@eadress.de>
2023-01-11 07:53:34 +0100

    Update WebSoccer.class.php
    Update: class TemplateEngine
    - clearCache revised
    - Functions are provided system-wide at runtime like PHP functions and can be used like PHP functions or their integrated functions of the extensions

--- classes/WebSoccer.class.php
+++ classes/WebSoccer.class.php
@@ -3352,6 +3352,24 @@ class TemplateEngine {
 			delDir($_SERVER['DOCUMENT_ROOT'].'/cache');
 			mkdir($_SERVER['DOCUMENT_ROOT'].'/cache');}}
 	function getEnvironment() { return $this->_environment;}
+	class TemplateEngine {
+	private $_environment;
+	private $_skin;
+	function __construct(WebSoccer $env, I18n $i18n, ViewHandler $viewHandler = null) {
+		$this->_skin = $env->getSkin();
+		$this->_initTwig();
+		$this->_environment->addGlobal(I18N_GLOBAL_NAME, $i18n);
+		$this->_environment->addGlobal(ENVIRONMENT_GLOBAL_NAME, $env);
+		$this->_environment->addGlobal(SKIN_GLOBAL_NAME, $this->_skin);
+		$this->_environment->addGlobal(VIEWHANDLER_GLOBAL_NAME, $viewHandler);}
+	function loadTemplate($templateName) {return $this->_environment->loadTemplate($this->_skin->getTemplate($templateName));}
+	function clearCache() {
+		if (file_exists(CACHE_FOLDER)) {
+			//- Fatal error: Uncaught Error: Call to undefined method Twig\Environment::clearCacheFiles()
+			//- $this->_environment->clearCacheFiles();
+			delDir($_SERVER['DOCUMENT_ROOT'].'/cache');
+			mkdir($_SERVER['DOCUMENT_ROOT'].'/cache');}}
+	function getEnvironment() {return $this->_environment;}
 	function _initTwig() {
 		$twigConfig = array('cache' => CACHE_FOLDER,);
 		if(version_compare(PHP_VERSION, '7.1.0') >= 0){
@@ -3362,7 +3380,13 @@ class TemplateEngine {
 			require_once($_SERVER['DOCUMENT_ROOT'].'/lib/1/Twig/Autoloader.php');
 			Twig_Autoloader::register();
 			$loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'].'/templates/default');
-			$this->_environment = new Twig_Environment($loader, $twigConfig);} }
+			$this->_environment = new Twig_Environment($loader, $twigConfig);}
+		$this->_environment->registerUndefinedFilterCallback(function($name){
+			if(function_exists($name)){return new Twig_SimpleFilter($name,function()use($name){return call_user_func_array($name,func_get_args());});}
+			return false;});
+		$this->_environment->registerUndefinedFunctionCallback(function($name){
+			if(function_exists($name)){return new Twig_SimpleFunction($name,function()use($name){return call_user_func_array( $name,func_get_args());});}
+			return false;});}
 	function _addSettingsSupport() {
 		$function = new Twig_SimpleFunction(CONFIG_FUNCTION_NAME, function ($key) {
 			global $i18n;
