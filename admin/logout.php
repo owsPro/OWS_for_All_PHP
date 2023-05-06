b5a4b132c6ee3642aa21384529c6f4eb96b54a08
owsPro <elm@eadress.de>
2023-05-06 22:17:37 +0200

    Update logout.php
    If you log out of the Admin Centre successfully, a corresponding text appears. If the language used could not be determined or is not yet fully supported, a corresponding txt is now output and the default language is set to 'de'.

--- admin/logout.php
+++ admin/logout.php
@@ -1,7 +1,7 @@
 <?php
 /*This file is part of "OWS for All PHP" (Rolf Joseph)
   https://github.com/owsPro/OWS_for_All_PHP/
-  A spinn-off for PHP Versions 5.4 to 8.2 from:
+  A spinn-off for PHP Versions 5.5 to 8.2 from:
   OpenWebSoccer-Sim(Ingo Hofmann), https://github.com/ihofmann/open-websoccer.
 
   "OWS for All PHP" is is distributed in WITHOUT ANY WARRANTY;
@@ -11,6 +11,11 @@
   See GNU Lesser General Public License Version 3 http://www.gnu.org/licenses/
 
 *****************************************************************************/
-require_once(__DIR__ .'/..' . '/admin/config/global.inc.php');
+require_once(__DIR__.'/..'.'/admin/config/global.inc.php');
 SecurityUtil::logoutAdmin();
-header('location: login.php?loggedout=1');
+$lang = locale_accept_from_http($_SERVER['HTTP_ACCEPT_LANGUAGE']);											// Get the preferred language from the browser
+$supported=['en','de','es'];																				// Define a list of supported languages
+if(in_array($lang,$supported))																				// Check if the preferred language is supported
+	header('location:login.php?loggedout=1&lang=$lang');													// Execute the code for the preferred language
+echo'<pre>										<b>This language is not yet fully supported!</b></pre>';	// Echo unsupported message
+echo"<meta http-equiv='refresh' content='2;URL=login.php?loggedout=1&lang=de'/>";							// Execute the code for the standard language
