0ad57619dee96340132d68f7e325d215f9aa7930
owsPro <elm@eadress.de>
2023-07-17 07:30:50 +0200

    Advanced: Start screen of the Admin Center
    You can now change the language after logging in to the Admin Center by clicking 2 times on the country flag.

--- admin/pages/home.php
+++ admin/pages/home.php
@@ -1,61 +1,44 @@
 <?php
-/******************************************************
+/*This file is part of "OWS for All PHP" (Rolf Joseph)
+  https://github.com/owsPro/OWS_for_All_PHP/
+  A spinn-off for PHP Versions 5.4 to 8.2 from:
+  OpenWebSoccer-Sim(Ingo Hofmann), https://github.com/ihofmann/open-websoccer.
 
-  This file is part of OpenWebSoccer-Sim.
+  "OWS for All PHP" is is distributed in WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.
 
-  OpenWebSoccer-Sim is free software: you can redistribute it 
-  and/or modify it under the terms of the 
-  GNU Lesser General Public License 
-  as published by the Free Software Foundation, either version 3 of
-  the License, or any later version.
+  See GNU Lesser General Public License Version 3 http://www.gnu.org/licenses/
 
-  OpenWebSoccer-Sim is distributed in the hope that it will be
-  useful, but WITHOUT ANY WARRANTY; without even the implied
-  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-  See the GNU Lesser General Public License for more details.
+*****************************************************************************/
+if(!$show){?>
+	<a href="/admin/index.php?lang=de"><img src="/img/flags/de.png" width="24" height="24" alt="deutsch" title="deutsch" /></a>
+	<a href="/admin/index.php?lang=en"><img src="/img/flags/en.png" width="24" height="24" alt="english" title="english" /></a>
+	<a href="/admin/index.php?lang=es"><img src="/img/flags/es.png" width="24" height="24" alt="español" title="español" /></a>
+	<a href="/admin/index.php?lang=pt"><img src="/img/flags/pt.png" width="24" height="24" alt="português" title="português" /></a>
+	<a href="/admin/index.php?lang=dk"><img src="/img/flags/dk.png" width="24" height="24" alt="dansk" title="dansk" /></a>
+	<a href="/admin/index.php?lang=ee"><img src="/img/flags/ee.png" width="24" height="24" alt="eesti" title="eesti" /></a>
+	<a href="/admin/index.php?lang=fi"><img src="/img/flags/fi.png" width="24" height="24" alt="suomalainen" title="suomalainen" /></a>
+	<a href="/admin/index.php?lang=fr"><img src="/img/flags/fr.png" width="24" height="24" alt="français" title="français" /></a>
+	<a href="/admin/index.php?lang=id"><img src="/img/flags/id.png" width="24" height="24" alt="indonesia" title="indonesia" /></a>
+	<a href="/admin/index.php?lang=it"><img src="/img/flags/it.png" width="24" height="24" alt="italiano" title="italiano" /></a>
+	<a href="/admin/index.php?lang=lv"><img src="/img/flags/lv.png" width="24" height="24" alt="latvieši" title="latvieši" /></a>
+	<a href="/admin/index.php?lang=lt"><img src="/img/flags/lt.png" width="24" height="24" alt="lietuviškas" title="lietuviškas" /></a>
+	<a href="/admin/index.php?lang=nl"><img src="/img/flags/nl.png" width="24" height="24" alt="nederlands" title="nederlands" /></a>
+	<a href="/admin/index.php?lang=pl"><img src="/img/flags/pl.png" width="24" height="24" alt="polska" title="polska" /></a>
 
-  You should have received a copy of the GNU Lesser General Public 
-  License along with OpenWebSoccer-Sim.  
-  If not, see <http://www.gnu.org/licenses/>.
+	<a href="/admin/index.php?lang=br"><img src="/img/flags/br.png" width="24" height="24" alt="???lang_label_br???" title="???lang_label_br???" /></a>
+	<a href="/admin/index.php?lang=ro"><img src="/img/flags/ro.png" width="24" height="24" alt="???lang_label_ro???" title="???lang_label_ro???" /></a>
+	<a href="/admin/index.php?lang=se"><img src="/img/flags/se.png" width="24" height="24" alt="???lang_label_se???" title="???lang_label_se???" /></a>
 
-******************************************************/
+	<a href="/admin/index.php?lang=sk"><img src="/img/flags/sk.png" width="24" height="24" alt="???lang_label_sk???" title="???lang_label_sk???" /></a>
+	<a href="/admin/index.php?lang=si"><img src="/img/flags/si.png" width="24" height="24" alt="???lang_label_si???" title="???lang_label_si???" /></a>
+	<a href="/admin/index.php?lang=cz"><img src="/img/flags/cz.png" width="24" height="24" alt="???lang_label_cz???" title="???lang_label_cz???" /></a>
 
-if (!$show) {
-
-  ?>
-
-  <h1><?php echo sprintf($i18n->getMessage('home_title'), escapeOutput($admin['name'])); ?></h1>
-
-  <p><?php echo $i18n->getMessage('home_intro'); ?></p>
-
-  <h3><?php echo $i18n->getMessage('home_softwareinfo_title'); ?></h3>
-  
- <table class='table table-bordered' style='width: 500px;'>
-  <tr>
-	<td><b><?php echo $i18n->getMessage('home_softwareinfo_name'); ?></b></td>
-	<td>OpenWebSoccer-Sim</td>
-  </tr>
-  <tr>
-	<td><b><?php echo $i18n->getMessage('home_softwareinfo_version'); ?></b></td>
-	<td><?php readfile('config/version.txt'); ?></td>
-  </tr>
-</table> 
-
-  <h3><?php echo $i18n->getMessage('home_projectinfo_title'); ?></h3>
-
-        <table class='table table-bordered' style='width: 500px;'>
-          <tr>
-            <td><b><?php echo $i18n->getMessage('home_projectinfo_name'); ?></b></td>
-            <td><?php echo escapeOutput($website->getConfig('projectname')) ?></td>
-          </tr>
-          <tr>
-            <td><b><?php echo $i18n->getMessage('home_projectinfo_adminemail'); ?></b></td>
-            <td><a href='mailto:<?php echo $website->getConfig('systememail'); ?>'><?php echo $website->getConfig('systememail'); ?></a></td>
-          </tr>
-        </table>
-
-  <?php
-
-}
-
-?>
+	<a href="/admin/index.php?lang=tr"><img src="/img/flags/tr.png" width="24" height="24" alt="???lang_label_tr???" title="???lang_label_tr???" /></a>
+	<a href="/admin/index.php?lang=hu"><img src="/img/flags/hu.png" width="24" height="24" alt="???lang_label_hu???" title="???lang_label_hu???" /></a>
+	<a href="/admin/index.php?lang=jp"><img src="/img/flags/jp.png" width="24" height="24" alt="???lang_label_jp???" title="???lang_label_jp???" /></a><br><br>
+<h1><?php echo sprintf($i18n->getMessage('home_title'),escapeOutput($admin['name']));?></h1><p><?php echo$i18n->getMessage('home_intro');?></p><h3><?php echo$i18n->getMessage('home_softwareinfo_title');?></h3>
+<table class='table table-bordered'style='width:500px;'><tr><td><b><?php echo$i18n->getMessage('home_softwareinfo_name');?></b></td><td>owsPro for All PHP</td></tr><tr><td><b><?php echo$i18n->getMessage('home_softwareinfo_version');?></b></td>
+<td><?php readfile('config/version.txt');?></td></tr></table><h3><?php echo$i18n->getMessage('home_projectinfo_title');?></h3><table class='table table-bordered'style='width:500px;'><tr><td><b><?php echo$i18n->getMessage('home_projectinfo_name');?></b></td>
+<td><?php echo escapeOutput(Config('projectname'))?></td></tr><tr><td><b><?php echo$i18n->getMessage('home_projectinfo_adminemail');?></b></td><td><a href='mailto:<?php echo Config('systememail');?>'><?php echo Config('systememail');?></a></td></tr></table><?php }
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+if(version_compare(PHP_VERSION, '5.4.0') < 0){echo'Minimum PHP 5.4.0 !';exit;}
 /*This file is part of "OWS for All PHP" (Rolf Joseph)
   https://github.com/owsPro/OWS_for_All_PHP/
-  A spinn-off for PHP Versions 5.5 to 8.2 from:
+  A spinn-off for PHP Versions 5.4 to 8.2 from:
   OpenWebSoccer-Sim(Ingo Hofmann), https://github.com/ihofmann/open-websoccer.
 
   "OWS for All PHP" is is distributed in WITHOUT ANY WARRANTY;
@@ -11,61 +12,221 @@
   See GNU Lesser General Public License Version 3 http://www.gnu.org/licenses/
 
 *****************************************************************************/
-if (version_compare(PHP_VERSION, '5.5.0') < 0){echo'Minimum PHP 5.5.0 !';exit;}
-include($_SERVER['DOCUMENT_ROOT']. '/frontbase.inc.php');
-$isOffline = FALSE;
-if ($website->getConfig('offline') == 'offline') $isOffline = TRUE;
-else {
-	$offlineTimeSpansConfig = $website->getConfig('offline_times');
-	if (strlen($offlineTimeSpansConfig)) {
-		$timeSpans = explode(',', $offlineTimeSpansConfig);
-		$now = $website->getNowAsTimestamp();
-		foreach ($timeSpans as $timeSpan) {
-			$timeSpanElements = explode('-', trim($timeSpan));
-			if (count($timeSpanElements) != 2) throw new Exception('Configuration error: Recurring offline mode time span must have a format like 15:00-17:00.');
-			$fromTimestamp = strtotime('today ' . trim($timeSpanElements[0]));
-			$toTimestamp = strtotime('today ' . trim($timeSpanElements[1]));
-			if ($fromTimestamp <= $now && $now <= $toTimestamp) {
-				$isOffline = TRUE;
-				break;}}}}
-if ($isOffline) {
-	$parameters['offline_message'] = nl2br($website->getConfig('offline_text'));
-	echo $website->getTemplateEngine($i18n)->loadTemplate('views/offline')->render($parameters);}
-else {
-	if (!isset($_SESSION['badgechecked']) && $website->getUser()->getRole() == ROLE_USER && $website->getUser()->getClubId($website, $db)) {
-		$userId = $website->getUser()->id;
-		$result = $db->querySelect('datum_anmeldung', $website->getConfig('db_prefix') . '_user', 'id = %d', $userId);
-		$userinfo = $result->fetch_array();
-		if ($userinfo['datum_anmeldung']) {
-			$numberOfRegisteredDays = round(($website->getNowAsTimestamp() - $userinfo['datum_anmeldung']) / (3600 * 24));
-			BadgesDataService::awardBadgeIfApplicable($website, $db, $userId, 'membership_since_x_days', $numberOfRegisteredDays);}
-		$_SESSION['badgechecked'] = 1;}
-	$pageId = $website->getRequestParameter(PARAM_PAGE);
-	$pageId = PageIdRouter::getTargetPageId($website, $i18n, $pageId);
-	$website->setPageId($pageId);
-	$validationMessages = null;
-	$actionId = $website->getRequestParameter(PARAM_ACTION);
-	if ($actionId !== NULL) {
-		try{$targetId = ActionHandler::handleAction($website, $db, $i18n, $actionId);
-			if ($targetId != null) $pageId = $targetId;}
-		catch (ValidationException $ve) {
-			$validationMessages = $ve->getMessages();
-			$website->addFrontMessage(new FrontMessage(MESSAGE_TYPE_ERROR, $i18n->getMessage('validation_error_box_title'), $i18n->getMessage('validation_error_box_message')));}
-		catch (Exception $e) { $website->addFrontMessage(new FrontMessage(MESSAGE_TYPE_ERROR, $i18n->getMessage('errorpage_title'), $e->getMessage()));}}
-	$website->setPageId($pageId);
-	$navItems = NavigationBuilder::getNavigationItems($website, $i18n, $page, $pageId);
-	$parameters['navItems'] = $navItems;
-	$parameters['breadcrumbItems'] = BreadcrumbBuilder::getBreadcrumbItems($website, $i18n, $page, $pageId);
-	header('Content-type: text/html; charset=utf-8');
-	$viewHandler = new ViewHandler($website, $db, $i18n, $page, $block, $validationMessages);
-	try{print_r($viewHandler->handlePage($pageId, $parameters));}
-	catch (AccessDeniedException $e) {
-		if ($website->getUser()->getRole() == ROLE_GUEST) {
-			$website->addFrontMessage(new FrontMessage(MESSAGE_TYPE_ERROR, $e->getMessage(), ''));
-			print_r($viewHandler->handlePage('login', $parameters));}
-		else renderErrorPage($website, $i18n, $viewHandler, $e->getMessage(), $parameters);}
-	catch (Exception $e) {renderErrorPage($website, $i18n, $viewHandler, $e->getMessage(), $parameters);}}
-function renderErrorPage($website, $i18n, $viewHandler, $message, $parameters) {
-	$parameters['title'] = $message;
-	$parameters['message'] = '';
-	print_r($website->getTemplateEngine($i18n, $viewHandler)->loadTemplate('error')->render($parameters));}
+
+define('BASE_FOLDER', __DIR__ .'/..');
+
+include(BASE_FOLDER . '/admin/adminglobal.inc.php');
+if (isset($_GET['lang'])) {
+	$i18n->setCurrentLanguage($_GET['lang']);
+}
+// building nav
+$navItems['settings'] = array();
+$navItems['website'] = array();
+
+foreach ($adminpage as $pageId => $pageData) {
+	$pageInfo = json_decode($pageData, true);
+
+	// check permission
+	if ((!isset($admin['r_admin']) || !$admin['r_admin']) && (!isset($admin['r_demo']) || !$admin['r_demo'])
+			&& (!isset($admin[$pageInfo['permissionrole']]) || !$admin[$pageInfo['permissionrole']])) {
+		continue;
+	}
+
+	if (isset($pageInfo['entity']) && $pageInfo['entity']) {
+		$siteInfo['label'] = $i18n->getMessage('entity_' . $pageInfo['entity']);
+		$siteInfo['pageid'] = 'manage';
+		$siteInfo['entity'] = $pageInfo['entity'];
+	} else {
+		$siteInfo['label'] = $i18n->getNavigationLabel($pageId);
+		$siteInfo['pageid'] = $pageInfo['filename'];
+		$siteInfo['entity'] = null;
+	}
+
+	$navItems[$pageInfo['navcategory']][] = $siteInfo;
+}
+
+function printNavItem($currentSite, $pageId, $navLabel, $entity = '') {
+
+	$url = '?site='. $pageId;
+	$active = ($currentSite == $pageId);
+
+	if (strlen($entity)) {
+		$url .= '&entity=' . escapeOutput($entity);
+		$active = (isset($_REQUEST['entity']) &&  $_REQUEST['entity'] == $entity);
+	}
+
+	echo '<li';
+	if ($active) echo ' class=\'active\'';
+	echo '><a href=\''. $url . '\'>'. $navLabel . '</a></li>';
+}
+
+if(isset($_GET['DBSave']))DBSave();?><a href='index.php?DBSave=true'><pre>	DBSave</a><?php if(isset($_GET['Diagnosis']))Diagnosis();?><a href='index.php?Diagnosis=true'>	Diagnosis</pre></a>
+<!DOCTYPE html>
+<html lang="<?php echo $i18n->getCurrentLanguage(); ?>">
+  <head>
+    <title><?php echo $i18n->getMessage("main_title")?></title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="bootstrap-datepicker/css/datepicker.css" rel="stylesheet">
+    <link href="bootstrap-timepicker/css/bootstrap-timepicker.min.css" rel="stylesheet" >
+    <link href="select2/select2.css" rel="stylesheet"/>
+    <link href="markitup/skins/simple/style.css" rel="stylesheet" />
+	<link href="markitup/sets/ws/style.css" rel="stylesheet" />
+	<link href="bootstrap/bootstrap-tag.css" rel="stylesheet" >
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+
+      .cupround {
+			margin-left: 30px;
+			border-left: 1px solid #CCCCCC;
+			padding: 3px 5px 0px 10px;
+		}
+    </style>
+  </head>
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="index.php" title="<?php echo $i18n->getMessage("admincenter_homelink_tooltip"); ?>"><?php echo $i18n->getMessage("admincenter_brand") ?></a>
+          <div class="nav-collapse collapse">
+            <p class="navbar-text pull-right">
+              <?php echo $i18n->getMessage("admincenter_loggedin_as"); ?> <a href="?site=profile" class="navbar-link" title="<?php echo $i18n->getMessage("admincenter_editprofile_tooltip"); ?>"><?php echo escapeOutput($admin['name']); ?></a> (<a href="logout.php" class="navbar-link"><?php echo $i18n->getMessage("admincenter_logout"); ?></a>)
+            </p>
+            <ul class="nav">
+              <li><a href="<?php
+              $contextRoot = $website->getConfig("context_root");
+              echo  (strlen($contextRoot)) ? $contextRoot : "/"; ?>"><i class="icon-globe icon-white"></i> <?php echo $i18n->getMessage("admincenter_link_website"); ?></a></li>
+			  <li><a href="?site=profile"><i class="icon-user icon-white"></i> <?php echo $i18n->getMessage("admincenter_link_profile"); ?></a></li>
+			  <li><a href="?site=clearcache"><i class="icon-refresh icon-white"></i> <?php echo $i18n->getMessage("admincenter_link_clear_cache"); ?></a></li>
+			  <li><a href="logout.php"><i class="icon-off icon-white"></i> <?php echo $i18n->getMessage("admincenter_logout"); ?></a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span2">
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+
+			  <?php
+				foreach ($navItems as $navCategory => $categoryItems) {
+					echo "<li class=\"nav-header\">". $i18n->getNavigationLabel("category_" . $navCategory) . "</li>";
+					foreach ($categoryItems as $navInfo) {
+						printNavItem($site, $navInfo["pageid"], $navInfo["label"], $navInfo["entity"]);
+					}
+				}
+			  ?>
+            </ul>
+          </div><!--/.well -->
+        </div><!--/span-->
+        <div class="span10">
+
+        	<div id="ajaxSpinner" style="display: none">
+        		<img src="../img/ajax-loader.gif" width="16" height="16" />
+        	</div>
+<?php
+if (empty($site)) {
+	$site = 'home';
+}
+
+$includeFile = 'pages/' . $site .'.php';
+if (preg_match('#^[a-z0-9_-]+$#i', $site) && file_exists($includeFile) ) {
+	try {
+		include( $includeFile );
+	} catch(Exception $e) {
+		echo createErrorMessage($i18n->getMessage('alert_error_title'), $e->getMessage());
+	}
+} else {
+	echo createErrorMessage($i18n->getMessage('alert_error_title'), $i18n->getMessage('error_page_not_found'));
+}
+?>
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <hr>
+
+      <footer>
+        <p>Powered by <a href="http://www.websoccer-sim.com" target="_blank">OpenWebSoccer-Sim</a></p>
+      </footer>
+	</div>
+
+
+    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+    <script src="bootstrap-datepicker/js/locales/bootstrap-datepicker.<?php echo $i18n->getCurrentLanguage(); ?>.js"></script>
+    <script src="bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
+	<script src="select2/select2.min.js"></script>
+	<?php
+	if ($i18n->getCurrentLanguage() != "en") {
+		echo "<script src=\"select2/select2_locale_". $i18n->getCurrentLanguage() . ".js\"></script>";
+	}
+	?>
+
+	<script src="markitup/jquery.markitup.js"></script>
+
+	<?php if ($i18n->getCurrentLanguage() == "de") { ?>
+		<script src="markitup/sets/ws/set_de.js"></script>
+	<?php } else { ?>
+		<script src="markitup/sets/ws/set.js"></script>
+	<?php } ?>
+	<script src="js/admincenter.js"></script>
+
+	<script src="js/bootbox.min.js"></script>
+
+	<script src="js/bootstrap-tag.js"></script>
+
+	<script>
+	$(function() {
+		$(document).on("click", ".deleteBtn", function(e) {
+			bootbox.confirm("<?php echo $i18n->getMessage("manage_delete_multiselect_confirm"); ?>",
+					"<?php echo $i18n->getMessage("option_no"); ?>",
+					"<?php echo $i18n->getMessage("option_yes"); ?>",
+			function(result) {
+				if (result) {
+					document.frmMain.submit();
+				}
+
+			});
+		});
+		$(document).on("click", ".deleteLink", function(e) {
+			e.preventDefault();
+
+			var link = $(this);
+
+			bootbox.confirm("<?php echo $i18n->getMessage("manage_delete_link_confirm"); ?>",
+					"<?php echo $i18n->getMessage("option_no"); ?>",
+					"<?php echo $i18n->getMessage("option_yes"); ?>",
+			function(result) {
+				if (result) {
+					window.location = link.attr("href");
+				}
+
+			});
+		});
+		$(".datepicker").datepicker({
+			format: "<?php echo str_replace("Y", "yyyy", $website->getConfig("date_format")); ?>",
+			language: "<?php echo $i18n->getCurrentLanguage(); ?>",
+			autoclose: true
+		});
+	});
+</script>
+
+  </body>
+</html>
