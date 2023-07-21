e1fd6ed5f8c5521d0651a469194c429bc68a9774
owsPro <elm@eadress.de>
2023-07-21 21:22:16 +0200

    Update index.php

--- install/index.php
+++ install/index.php
@@ -37,7 +37,7 @@ $supportedLanguages=["de"=>"deutsch","en"=>"english","es"=>"español","pt"=>"por
 		<div class="container">
 			<h1>owsPro Installation</h1>
 			<hr>
-			<?php if(file_exists(CONFIGFILE)){echo '<b>There is already a configuration. Please delete or rename the config.inc.php file in the "generated" directory.';exit();}
+			<?php
 				$errors=[];
 				$messagesIncluded=FALSE;
 				if(isset($_SESSION["lang"])){
