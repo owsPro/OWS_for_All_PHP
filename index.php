a7a7863004f6fd5dcdd5fcbd4828b6e6d5288e76
owsPro <elm@eadress.de>
2023-08-05 22:57:34 +0200

    Revised: Admin Center display
    - Removed the black horizontal navigation bar.
    - The left menu bar now also contains the menu items of the above navigation bar, as well as the previous separate new entries.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*This file is part of "OWS for All PHP" (Rolf Joseph)
   https://github.com/owsPro/OWS_for_All_PHP/
-  A spinn-off for PHP Versions 5.5 to 8.2 from:
+  A spinn-off for PHP Versions 5.4 to 8.2 from:
   OpenWebSoccer-Sim(Ingo Hofmann), https://github.com/ihofmann/open-websoccer.
 
   "OWS for All PHP" is is distributed in WITHOUT ANY WARRANTY;
@@ -11,16 +11,44 @@
   See GNU Lesser General Public License Version 3 http://www.gnu.org/licenses/
 
 *****************************************************************************/
-	if(version_compare(PHP_VERSION,'5.5.0')<0){echo'Minimum PHP 5.5.0 !';exit;}include($_SERVER['DOCUMENT_ROOT'].'/frontbase.inc.php');$isOffline=FALSE;if(Config('offline')=='offline')$isOffline=TRUE;else{$offlineTimeSpansConfig=Config('offline_times');
-	if(strlen($offlineTimeSpansConfig)){$timeSpans=explode(',',$offlineTimeSpansConfig);$now=$website->getNowAsTimestamp();foreach($timeSpans as$timeSpan){$timeSpanElements=explode('-',trim($timeSpan));if(count($timeSpanElements)!=2)
-	throw new Exception('Configuration error: Recurring offline mode time span must have a format like 15:00-17:00.');$fromTimestamp=strtotime('today '.trim($timeSpanElements[0]));$toTimestamp=strtotime('today '.trim($timeSpanElements[1]));
-	if($fromTimestamp<=$now&&$now<=$toTimestamp){$isOffline=TRUE;break;}}}}if($isOffline){$parameters['offline_message']=nl2br(Config('offline_text'));echo $website->getTemplateEngine($i18n)->loadTemplate('views/offline')->render($parameters);}
-	else{if(!isset($_SESSION['badgechecked'])&&$website->getUser()->getRole()=='user'&&$website->getUser()->getClubId($website,$db)){$userId=$website->getUser()->id;$result=$db->querySelect('datum_anmeldung',Config('db_prefix').'_user','id=%d',$userId);
-	$userinfo=$result->fetch_array();if($userinfo['datum_anmeldung']){$numberOfRegisteredDays=round(($website->getNowAsTimestamp()-$userinfo['datum_anmeldung'])/(3600*24));BadgesDataService::awardBadgeIfApplicable($website,$db,$userId,'membership_since_x_days',
-	$numberOfRegisteredDays);}$_SESSION['badgechecked']=1;}$pageId=Request('page');$pageId=PageIdRouter::getTargetPageId($website,$i18n,$pageId);$website->setPageId($pageId);$validationMessages=null;$actionId=
-	Request('action');if($actionId!==NULL){try{$targetId=ActionHandler::handleAction($website,$db,$i18n,$actionId);if($targetId!=null)$pageId=$targetId;}catch(ValidationException$ve){$validationMessages=$ve->getMessages();
-	$website->addFrontMessage(new FrontMessage('error',Message('validation_error_box_title'),Message('validation_error_box_message')));}catch(Exception$e){$website->addFrontMessage(new FrontMessage('error',Message('errorpage_title'),
-	$e->getMessage()));}}$website->setPageId($pageId);$navItems=NavigationBuilder::getNavigationItems($website,$i18n,$page,$pageId);$parameters['navItems']=$navItems;$parameters['breadcrumbItems']=BreadcrumbBuilder::getBreadcrumbItems($website,$i18n,$page,$pageId);
-	header('Content-type:text/html;charset=utf-8');$viewHandler=new ViewHandler($website,$db,$i18n,$page,$block,$validationMessages);try{print_r($viewHandler->handlePage($pageId,$parameters));}catch(AccessDeniedException$e){if($website->getUser()->getRole()==
-	'guest'){$website->addFrontMessage(new FrontMessage('error',$e->getMessage(),''));print_r($viewHandler->handlePage('login',$parameters));}else renderErrorPage($website,$i18n,$viewHandler,$e->getMessage(),$parameters);}catch (Exception $e){
-	renderErrorPage($website,$i18n,$viewHandler,$e->getMessage(),$parameters);}}
+								if(version_compare(PHP_VERSION,'5.5.0')<0){echo'Minimum PHP 5.5.0 !';exit;}include($_SERVER['DOCUMENT_ROOT'].'/admin/adminglobal.inc.php');if(isset($_GET['lang']))$i18n->setCurrentLanguage($_GET['lang']);$navItems['settings']=[];
+								$navItems['website']=[];foreach($adminpage as$pageId=>$pageData){$pageInfo=json_decode($pageData,true);if((!isset($admin['r_admin'])||!$admin['r_admin'])&&(!isset($admin['r_demo'])||!$admin['r_demo'])&&(!isset($admin[
+								$pageInfo['permissionrole']])||!$admin[$pageInfo['permissionrole']]))continue;if(isset($pageInfo['entity'])&&$pageInfo['entity']){$siteInfo['label']=Message('entity_'.$pageInfo['entity']);$siteInfo['pageid']='manage';
+								$siteInfo['entity']=$pageInfo['entity'];}else{$siteInfo['label']=$i18n->getNavigationLabel($pageId);$siteInfo['pageid']=$pageInfo['filename'];$siteInfo['entity']=null;}$navItems[$pageInfo['navcategory']][]=$siteInfo;}Bootstrap_css();?>
+								<!DOCTYPE html><html lang='<?php echo escapeOutput($i18n->getCurrentLanguage());?>'><head><title><?php echo Message('main_title')?></title>								
+								<link href='bootstrap-datepicker/css/datepicker.css'rel='stylesheet'><link href='bootstrap-timepicker/css/bootstrap-timepicker.min.css'rel='stylesheet'>
+								<link href='select2/select2.css'rel='stylesheet'/><link href='markitup/skins/simple/style.css'rel='stylesheet'/>
+								<link href='markitup/sets/ws/style.css'rel='stylesheet'/><link href='bootstrap/bootstrap-tag.css'rel='stylesheet'><meta charset='UTF-8'>
+								<link rel='shortcut icon'type='image/x-icon'href='../favicon.ico'/><div class='container-fluid'><div class='row-fluid'><div class='span2'><br><div class='well sidebar-nav'><ul class='nav'><h3><a class='brand'href='index.php'title='
+								<?php echo Message('admincenter_homelink_tooltip');?>'><?php echo Message('admincenter_brand')?></a></h3><div class='nav-collapse collapse'><p class='navbar-text pull-right'></a></p><ul class='nav'><?php echo 
+								Message('admincenter_loggedin_as');echo' ';echo escapeOutput($admin['name']);?><br><br><li><a href="<?php $contextRoot=Config('context_root');echo(strlen($contextRoot))?$contextRoot:'/';?>"><i class='icon-globe icon-white'></i>
+								<?php echo Message('admincenter_link_website');?></a></li><li><a href='?site=profile'><i class='icon-user icon-white'></i> <?php echo Message('admincenter_link_profile');?></a></li><li><a href='?site=clearcache'>
+								<i class='icon-refresh icon-white'></i><?php echo Message('admincenter_link_clear_cache');?></a></li><li><a href='logout.php'><i class='icon-off icon-white'></i><?php echo Message('admincenter_logout');?></a></li><br><li><?php
+			  					if(isset($_GET['DBSave']))DBSave();?><a href='index.php?DBSave=true'>DBSave</a><li><li><?php if(isset($_GET['Diagnosis']))Diagnosis();?><a href='index.php?Diagnosis=true'>Diagnosis</a></li><?php foreach(
+			  					$navItems as$navCategory=>$categoryItems){echo'<li class=\'nav-header\'>'.$i18n->getNavigationLabel('category_'.$navCategory).'</li>';foreach($categoryItems as$navInfo)printNavItem($site,$navInfo['pageid'],$navInfo['label'],
+			  					$navInfo['entity']);}?></ul></div></div></div><br><div class="span10"><?php if(empty($site))$site='home';$includeFile='pages/'.$site.'.php';if(preg_match('#^[a-z0-9_-]+$#i',$site)&&file_exists($includeFile)){try{include($includeFile);}
+			  					catch(Exception$e){echo createErrorMessage(Message('alert_error_title'),$e->getMessage());}}else echo createErrorMessage(Message('alert_error_title'),Message('error_page_not_found'));?></div></div></div><div class='span2'><footer>
+        						<p>Powered by <a href='https://github.com/owsPro/owsPro' target='_blank'>owsPro</a><br>Forked from <a href='https://github.com/ihofmann/open-websoccer' target='_blank'>ihofmann</a></p></footer></div>
+    							<script src="https://code.jquery.com/jquery-latest.min.js"></script><script src="bootstrap/js/bootstrap.min.js"></script><script src="bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+    							<script src="bootstrap-datepicker/js/locales/bootstrap-datepicker.<?php echo escapeOutput($i18n->getCurrentLanguage());?>.js"></script><script src="bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
+								<script src="select2/select2.min.js"></script><?php if($i18n->getCurrentLanguage()!="en")echo"<script src=\"select2/select2_locale_".escapeOutput($i18n->getCurrentLanguage()).".js\"></script>";?>
+								<script src="markitup/jquery.markitup.js"></script><?php if ($i18n->getCurrentLanguage() == "de") { ?><script src="markitup/sets/ws/set_de.js"></script><?php } else ?><script src="markitup/sets/ws/set.js"></script><?php ?>
+								<script src="js/admincenter.js"></script><script src="js/bootbox.min.js"></script><script src="js/bootstrap-tag.js"></script><script>$(function(){$(document).on("click",".deleteBtn",function(e){bootbox.confirm("<?php echo 
+								Message("manage_delete_multiselect_confirm");?>","<?php echo Message("option_no");?>","<?php echo Message("option_yes");?>",function(result){if(result){document.frmMain.submit();}});});$(document).on("click", ".deleteLink",
+								function(e){e.preventDefault();var link=$(this);bootbox.confirm("<?php echo Message("manage_delete_link_confirm");?>","<?php echo Message("option_no");?>","<?php echo Message("option_yes");?>",function(result){
+								if(result)window.location=link.attr("href");}});});$(".datepicker").datepicker({format: "<?php echo str_replace("Y","yyyy",Config("date_format"));?>",language: "<?php echo escapeOutput($i18n->getCurrentLanguage());?>",autoclose: true});});
+								</script></body></html>
+			  					
+			  					
+			  					
+			  					
+			  					
+			  					
+			  					
+			  					
+			  					
+			  					
+			  					
+			  					
+			  					
+			  					
