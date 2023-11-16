0cc36faaf28b25920dcb68243eb75cdaf45fa7a6
owsPro <elm@eadress.de>
2023-11-16 18:23:36 +0100

    Update termsandconditions.php

--- admin/pages/termsandconditions.php
+++ admin/pages/termsandconditions.php
@@ -11,24 +11,23 @@
   See GNU Lesser General Public License Version 3 http://www.gnu.org/licenses/
 
 ******************************************************/
-
-$mainTitle =Message("termsandconditions_navlabel");
+$mainTitle = $i18n->getMessage("termsandconditions_navlabel");
 
 if (!$admin["r_admin"] && !$admin["r_demo"] && !$admin[$page["permissionrole"]]) {
-	throw new Exception(Message("error_access_denied"));
+	throw new Exception($i18n->getMessage("error_access_denied"));
 }
 
 // get XML config
 $selectedLang = (isset($_POST["lang"])) ? $_POST["lang"] : $i18n->getCurrentLanguage();
 
-$termsFile = $_SERVER['DOCUMENT_ROOT']. "/admin/config/termsandconditions.xml";
+$termsFile = BASE_FOLDER . "/admin/config/termsandconditions.xml";
 if (!file_exists($termsFile)) {
 	throw new Exception("File does not exist: " . $termsFile);
 }
 
 $xml = simplexml_load_file($termsFile);
 
-$termsConfig = escapeOutput($xml->xpath("//pagecontent[@lang = '". $selectedLang . "'][1]"));
+$termsConfig = $xml->xpath("//pagecontent[@lang = '". $selectedLang . "'][1]");
 if (!$termsConfig) {
 	throw new Exception("No terms and conditions available for this language. Create manually a new entry at " . $termsFile);
 }
@@ -39,12 +38,12 @@ if (!$show) {
 
   <h1><?php echo $mainTitle; ?></h1>
 
-  <p><?php echo Message("termsandconditions_introduction"); ?></p>
+  <p><?php echo $i18n->getMessage("termsandconditions_introduction"); ?></p>
 
-  <form action="<?php echo escapeOutput($_SERVER['PHP_SELF']); ?>" method="post" class="form-inline">
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form-inline">
 	<input type="hidden" name="site" value="<?php echo $site; ?>">
 
-	<label for="lang"><?php echo Message("termsandconditions_label_language"); ?></label>
+	<label for="lang"><?php echo $i18n->getMessage("termsandconditions_label_language"); ?></label>
 	<select name="lang" id="lang">
 		<?php
 		foreach($i18n->getSupportedLanguages() as $language) {
@@ -54,10 +53,10 @@ if (!$show) {
 		}
 		?>
 	</select>
-	<button type="submit" class="btn"><?php echo Message("button_display"); ?></button>
+	<button type="submit" class="btn"><?php echo $i18n->getMessage("button_display"); ?></button>
   </form>
 
-  <form action="<?php echo escapeOutput($_SERVER['PHP_SELF']); ?>" method="post" class="form-horizontal">
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="form-horizontal">
     <input type="hidden" name="show" value="save">
     <input type="hidden" name="lang" value="<?php echo escapeOutput($selectedLang); ?>">
 	<input type="hidden" name="site" value="<?php echo $site; ?>">
@@ -70,13 +69,13 @@ if (!$show) {
 
 	$formFields["content"] = array("type" => "html", "value" => $terms, "required" => "true");
 	foreach ($formFields as $fieldId => $fieldInfo) {
-		echo createFormGroup($i18n, $fieldId, $fieldInfo, $fieldInfo["value"], "imprint_label_");
+		echo FormBuilder::createFormGroup($i18n, $fieldId, $fieldInfo, $fieldInfo["value"], "imprint_label_");
 	}
 	?>
 	</fieldset>
 	<div class="form-actions">
-		<input type="submit" class="btn btn-primary" accesskey="s" title="Alt + s" value="<?php echo Message("button_save"); ?>">
-		<input type="reset" class="btn" value="<?php echo Message("button_reset"); ?>">
+		<input type="submit" class="btn btn-primary" accesskey="s" title="Alt + s" value="<?php echo $i18n->getMessage("button_save"); ?>">
+		<input type="reset" class="btn" value="<?php echo $i18n->getMessage("button_reset"); ?>">
 	</div>
   </form>
 
@@ -87,9 +86,9 @@ if (!$show) {
 //********** save **********
 elseif ($show == "save") {
 
-  if (!isset($_POST['content']) || !strlen($_POST['content'])) $err[] =Message("imprint_validationerror_content");
-  if (!is_writable($termsFile)) $err[] =Message("termsandconditions_err_filenotwritable", $termsFile);
-  if ($admin['r_demo']) $err[] =Message("validationerror_no_changes_as_demo");
+  if (!isset($_POST['content']) || !strlen($_POST['content'])) $err[] = $i18n->getMessage("imprint_validationerror_content");
+  if (!is_writable($termsFile)) $err[] = $i18n->getMessage("termsandconditions_err_filenotwritable", $termsFile);
+  if ($admin['r_demo']) $err[] = $i18n->getMessage("validationerror_no_changes_as_demo");
 
   if (isset($err)) {
 
@@ -98,7 +97,7 @@ elseif ($show == "save") {
   }
   else {
 
-    echo "<h1>". $mainTitle ." &raquo; ".Message("subpage_save_title") . "</h1>";
+    echo "<h1>". $mainTitle ." &raquo; ". $i18n->getMessage("subpage_save_title") . "</h1>";
 
     $termsContent = stripslashes($_POST['content']);
 
@@ -117,9 +116,9 @@ elseif ($show == "save") {
 
 	$xml->asXML($termsFile);
 
-	echo createSuccessMessage(Message("alert_save_success"), "");
+	echo createSuccessMessage($i18n->getMessage("alert_save_success"), "");
 
-    echo "<p>&raquo; <a href=\"?site=". $site ."\">".Message("back_label") . "</a></p>\n";
+    echo "<p>&raquo; <a href=\"?site=". $site ."\">". $i18n->getMessage("back_label") . "</a></p>\n";
 
   }
 
