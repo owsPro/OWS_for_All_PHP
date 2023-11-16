ee18a4bd7e7df5b5f6f8d71e6e785acd944308f9
owsPro <elm@eadress.de>
2023-11-16 12:26:09 +0100

    Update rss.php

--- webservices/rss.php
+++ webservices/rss.php
@@ -11,7 +11,46 @@
   See GNU Lesser General Public License Version 3 http://www.gnu.org/licenses/
 
 *****************************************************************************/
-	include($_SERVER['DOCUMENT_ROOT'].'/admin/config/global.inc.php');include($_SERVER['DOCUMENT_ROOT'].'/cache/wsconfigfront.inc.php');$i18n=I18n::getInstance(Config('supported_languages'));$lang=Request('lang');if($lang){
-	try{$i18n->setCurrentLanguage($lang);}catch(Exception$e){}}include(sprintf($_SERVER['DOCUMENT_ROOT'].'/cache/messages_%s.inc.php',$i18n->getCurrentLanguage()));include(sprintf($_SERVER['DOCUMENT_ROOT'].'/cache/entitymessages_%s.inc.php',$i18n->getCurrentLanguage()));
-	$pageId=Request('page');setPageId($pageId);header('Content-type:application/rss+xml;charset=utf-8');$viewHandler=new ViewHandler($website,$db,$i18n,$page,$block,null);try{echo$viewHandler->handlePage($pageId,[]);}
-	catch(Exception$e){echo$e->getMessage();}
+	include($_SERVER['DOCUMENT_ROOT'].'/admin/config/global.inc.php');
+include($_SERVER['DOCUMENT_ROOT'].'/cache/wsconfigfront.inc.php');
+
+// Create an instance of the I18n class with the supported languages configuration
+$i18n = I18n::getInstance(Config('supported_languages'));
+
+// Get the requested language from the request
+$lang = Request('lang');
+
+// If a language is specified, set it as the current language
+if ($lang) {
+    try {
+        $i18n->setCurrentLanguage($lang);
+    } catch (Exception $e) {
+        // Handle any exceptions that occur when setting the current language
+    }
+}
+
+// Include the language messages for the current language
+include(sprintf($_SERVER['DOCUMENT_ROOT'].'/cache/messages_%s.inc.php', $i18n->getCurrentLanguage()));
+
+// Include the entity messages for the current language
+include(sprintf($_SERVER['DOCUMENT_ROOT'].'/cache/entitymessages_%s.inc.php', $i18n->getCurrentLanguage()));
+
+// Get the requested page ID from the request
+$pageId = Request('page');
+
+// Set the page ID
+setPageId($pageId);
+
+// Set the response content type to RSS
+header('Content-type:application/rss+xml;charset=utf-8');
+
+// Create a new instance of the ViewHandler class
+$viewHandler = new ViewHandler($website, $db, $i18n, $page, $block, null);
+
+try {
+    // Handle the requested page and echo the result
+    echo $viewHandler->handlePage($pageId, []);
+} catch (Exception $e) {
+    // Handle any exceptions that occur during page handling
+    echo print_r($e->getMessage());
+}
